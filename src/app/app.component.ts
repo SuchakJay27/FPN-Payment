07040da494b2c6a00c3669ced607b0cafc3b2d60
SuchakJay27 <suchakjayu@gmail.com>
2025-05-13 16:21:55 +0530

    Accessibility and Lang Translation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,25 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './core/header/header.component';
 import { FooterComponent } from './core/footer/footer.component';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent,TranslateModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'FPN-Payment';
+
+    constructor(private translate: TranslateService) {
+       let  setlang = localStorage.getItem('appLang')
+       if (setlang) {
+         this.translate.setDefaultLang(setlang);
+       }else{
+         localStorage.setItem('appLang', 'en');
+         this.translate.setDefaultLang('en');
+       }
+  }
 }
--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+currentLang: string;
 
+  constructor(private translate: TranslateService) {
+    const savedLang = localStorage.getItem('appLang') || 'en';
+    this.currentLang = savedLang;
+    this.translate.use(this.currentLang);
+  }
+
+  ChangeLang() {
+    this.currentLang = this.currentLang === 'en' ? 'cymraeg' : 'en';
+    this.translate.use(this.currentLang);
+    localStorage.setItem('appLang', this.currentLang);
+  }
+
+  getToggleLangLabel(): string {
+    return this.currentLang === 'en' ? 'CYMRAEG' : 'ENGLISH';
+  }
 }
